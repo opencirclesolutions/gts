6f51ee68f7acf1623ef64d01ef32d403d9a97b36
wittekip <bas.rutten@opencirclesolutions.nl>
2018-10-25 10:33:19 +0200

    simple table component

--- gts-ui/src/main/java/com/ocs/gts/ui/OrganizationView.java
+++ gts-ui/src/main/java/com/ocs/gts/ui/OrganizationView.java
@@ -2,20 +2,30 @@ package com.ocs.gts.ui;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.google.gwt.thirdparty.guava.common.collect.Sets;
 import com.ocs.dynamo.domain.model.EntityModel;
-import com.ocs.dynamo.filter.AndPredicate;
 import com.ocs.dynamo.filter.LikePredicate;
+import com.ocs.dynamo.filter.OrPredicate;
 import com.ocs.dynamo.filter.PropertyPredicate;
 import com.ocs.dynamo.ui.component.EntityComboBox;
+import com.ocs.dynamo.ui.component.EntityComboBox.SelectMode;
+import com.ocs.dynamo.ui.component.EntityListSelect;
+import com.ocs.dynamo.ui.component.QuickAddEntityComboBox;
+import com.ocs.dynamo.ui.component.QuickAddListSelect;
+import com.ocs.dynamo.ui.component.TokenFieldSelect;
 import com.ocs.dynamo.ui.view.BaseView;
 import com.ocs.gts.domain.Organization;
+import com.ocs.gts.domain.Person;
 import com.ocs.gts.service.OrganizationService;
+import com.ocs.gts.service.PersonService;
 import com.vaadin.data.provider.SortOrder;
 import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
 import com.vaadin.shared.data.sort.SortDirection;
 import com.vaadin.spring.annotation.SpringView;
 import com.vaadin.spring.annotation.UIScope;
+import com.vaadin.ui.Button;
 import com.vaadin.ui.Layout;
+import com.vaadin.ui.Notification;
 
 @UIScope
 @SpringView(name = Views.ORGANIZATION_VIEW)
@@ -23,6 +33,9 @@ public class OrganizationView extends BaseView {
 
 	@Autowired
 	private OrganizationService organizationService;
+
+	@Autowired
+	private PersonService personService;
 
 	private static final long serialVersionUID = 3310122000037867336L;
 
@@ -38,13 +51,52 @@ public class OrganizationView extends BaseView {
 		// PropertyPredicate<Organization> filter = new
 		// EqualsPredicate<Organization>("name", "Coda Nostra");
 		PropertyPredicate<Organization> like = new LikePredicate<Organization>("name", "%a%", false);
+		OrPredicate<Organization> or = new OrPredicate<>(new LikePredicate<Organization>("name", "%ca%", false),
+				new LikePredicate<Organization>("name", "%co%", false));
 
 		EntityModel<Organization> em = getModelFactory().getModel(Organization.class);
-		EntityComboBox<Integer, Organization> combo = new EntityComboBox<>(em, null, organizationService, like,
+		EntityComboBox<Integer, Organization> combo = new EntityComboBox<>(em, null, organizationService, or,
 				new SortOrder<String>("name", SortDirection.ASCENDING));
 		main.addComponent(combo);
 
+		combo.setAdditionalFilter(new LikePredicate<Organization>("name", "%cod%", false));
+
 		EntityComboBox<Integer, Organization> fixed = new EntityComboBox<>(em, null, organizationService.findAll());
 		main.addComponent(fixed);
+
+		// filtered list select
+		EntityListSelect<Integer, Organization> listSelect = new EntityListSelect<>(em, null, organizationService, like,
+				new SortOrder<String>("name", SortDirection.ASCENDING));
+		main.addComponent(listSelect);
+
+		PropertyPredicate<Person> likePerson = new LikePredicate<Person>("firstName", "%a%", false);
+
+		EntityModel<Person> personModel = getModelFactory().getModel(Person.class);
+		QuickAddEntityComboBox<Integer, Person> personQuickAdd = new QuickAddEntityComboBox<>(personModel, null,
+				personService, SelectMode.FILTERED, null, false, null);
+		main.addComponent(personQuickAdd);
+
+		Person person1 = personService.findById(1);
+		personQuickAdd.setValue(person1);
+		Person person2 = personService.findById(2);
+
+		Button button = new Button("Test");
+		button.addClickListener(evt -> {
+			Person person = personQuickAdd.getValue();
+			if (person != null) {
+				Notification.show(person.getFullName(), Notification.Type.ERROR_MESSAGE);
+			}
+		});
+		main.addComponent(button);
+
+		QuickAddListSelect<Integer, Person> personListSelect = new QuickAddListSelect<Integer, Person>(personModel,
+				null, personService, null, true, 5);
+		main.addComponent(personListSelect);
+		personListSelect.setValue(Sets.newHashSet(person1, person2));
+
+		TokenFieldSelect<Integer, Person> tokenField = new TokenFieldSelect<>(personModel, null, personService, null,
+				true);
+		main.addComponent(tokenField);
+		tokenField.setValue(Sets.newHashSet(person1, person2));
 	}
 }
--- gts-ui/src/main/java/com/ocs/gts/ui/PersonView.java
+++ gts-ui/src/main/java/com/ocs/gts/ui/PersonView.java
@@ -1,9 +1,17 @@
 package com.ocs.gts.ui;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.ocs.dynamo.domain.model.EntityModel;
+import com.ocs.dynamo.ui.composite.table.ModelBasedGrid;
 import com.ocs.dynamo.ui.view.BaseView;
+import com.ocs.gts.domain.Organization;
+import com.ocs.gts.domain.Person;
+import com.ocs.gts.service.OrganizationService;
 import com.ocs.gts.service.PersonService;
+import com.vaadin.data.provider.ListDataProvider;
 import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
 import com.vaadin.spring.annotation.SpringView;
 import com.vaadin.spring.annotation.UIScope;
@@ -17,10 +25,28 @@ public class PersonView extends BaseView {
 
 	@Autowired
 	private PersonService personService;
+	
+	@Autowired
+	private OrganizationService organizationService;
 
 	@Override
 	public void enter(ViewChangeEvent event) {
 		Layout main = initLayout();
 
+		List<Person> persons = personService.fetch(null);
+		ListDataProvider<Person> provider = new ListDataProvider<>(persons);
+
+		EntityModel<Person> model = getModelFactory().getModel(Person.class);
+		ModelBasedGrid<Integer, Person> table = new ModelBasedGrid<>(provider, model, false);
+
+		main.addComponent(table);
+		
+		List<Organization> organizations = organizationService.fetch(null);
+		ListDataProvider<Organization> orgProvider = new ListDataProvider<>(organizations);
+
+		EntityModel<Organization> oModel = getModelFactory().getModel(Organization.class);
+		ModelBasedGrid<Integer, Organization> orgTable = new ModelBasedGrid<>(orgProvider, oModel, false);
+
+		main.addComponent(orgTable);
 	}
 }
